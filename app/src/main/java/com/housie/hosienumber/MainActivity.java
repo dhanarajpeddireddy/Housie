f4a0c36c309ae0f229bcd49f137b4e6deff85cdc
dana <roshan@Shrinidhis-MacBook-Air.local>
2020-08-11 17:07:27 +0530

    first commit with project

---
+++ app/src/main/java/com/housie/hosienumber/MainActivity.java
@@ -0,0 +1,14 @@
+package com.housie.hosienumber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
